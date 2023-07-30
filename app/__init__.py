8015d0bd42ad1f856cfa30875026eedcb4ba0773
Emerson Espinoza <xinayespinoza@gmail.com>
2023-07-29 20:00:37 -0400

    manejor de error 404

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # rutas
 @app.route("/")
 def index():
     return "Hola mundo con compilando!!"
-
+# ruta para paginas no encontradas 
+def pagina_no_encontrada(error):
+    return render_template('errores/404.html'), 404
 
 def inicializar_app(config):
     app.config.from_object(config)
+    app.register_error_handler(404, pagina_no_encontrada)
     return app
