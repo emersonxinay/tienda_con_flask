80d2c563082cdf7c918ba034b22e2ef8ff1753e1
Emerson Espinoza <xinayespinoza@gmail.com>
2023-08-19 17:06:30 -0400

    fix login mensajes alert

--- app/__init__.py
+++ app/__init__.py
@@ -43,7 +43,7 @@ def login():
         usuario_logeado = ModeloUsuario.login(db, usuario)
         if usuario_logeado != None :
             login_user(usuario_logeado)
-           
+            
             return redirect(url_for('index'))
         else:
             flash("Credenciales invalidas...")
